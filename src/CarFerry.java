0fb88537b00dce2a7cb1b062f8d40c80969fb2ce
David Olsson <d.e.d.davidolsson@gmail.com>
2018-11-23 14:05:34 +0100

    Added Jdoc and made trailer abstract

--- src/CarFerry.java
+++ src/CarFerry.java
@@ -1,22 +1,44 @@
 import java.awt.*;
 
+/**
+ * A Ferry that transports cars
+ */
+
 public class CarFerry extends Vehicle implements ICarTransporter {
 
     private boolean inDock;
     private CarTransporter transporter;
 
+    /**
+     * Initiates an instances of a CarFerry
+     * @param color The colour of the ferry
+     * @param modelName The modelname of the ferry
+     * @param capacity The number of cars the ferry and take
+     * @param x The start x coordinate of the ferry
+     * @param y The start y coordinate
+     */
     public CarFerry(Color color, String modelName, int capacity, double x, double y) {
         super(color, modelName, x, y);
         transporter = new CarTransporter(capacity);
     }
 
-
+    /**
+     * Loads a car onto the ferry, if the car is already loaded it will throw an exception
+     * @param car The car to be loaded
+     */
     @Override
     public void addCar(Car car) {
         if(inDock){
             transporter.addCar(car, movable);
+        }else{
+            throw new IllegalStateException("The ferry is not in a harbour");
         }
     }
+
+    /**
+     * Unloads a car from the ferry and returns a reference to the car, if the ferry is empty it will return null
+     * @return The unloaded car
+     */
 
     @Override
     public Car removeCar() {
@@ -27,12 +49,21 @@ public class CarFerry extends Vehicle implements ICarTransporter {
         return null;
     }
 
+
+    /**
+     * Docks the ferry and enables it to load cars
+     */
     public void dock() {
         if (movable.getSpeed() == 0) {
             inDock = true;
         }
     }
+    //TODO check if the ferry is close to a harbour
 
+
+    /**
+     * Undocks the ferry, disables the ability to load cars
+     */
     public void unDock() {
         if (movable.getSpeed() == 0) {
             inDock = false;
--- src/Trailer.java
+++ src/Trailer.java
@@ -1,18 +1,39 @@
-public class Trailer implements IMovable {
+/**
+ * An abstract trailer that can be attached to a truck
+ */
+public abstract class Trailer implements IMovable {
     private Truck truck;
     protected Movable movable;
 
+    /**
+     * Creates a trailer at a position
+     * @param x The start x coordinate
+     * @param y The start y coordinate
+     */
     public Trailer(double x, double y) {
         movable = new Movable(x, y);
     }
+
+    /**
+     * returns a reference to the truck that is attached to the trailer, can return null.
+     * @return
+     */
 
     public Truck getTruck() {
         return truck;
     }
 
+    /**
+     * Attaches truck to the trailer
+     * @param truck Reference to the attached truck
+     */
     public void setTruck(Truck truck) {
         this.truck = truck;
     }
+
+    /**
+     * Movies the trailer
+     */
 
     @Override
     public void move() {
@@ -21,11 +42,17 @@ public class Trailer implements IMovable {
         //this.movable.setRotation(truck.movable.getRotation());
     }
 
+    /**
+     * Turns the truck to the left
+     */
     @Override
     public void turnLeft() {
         this.movable.rotate(10);
     }
 
+    /**
+     * Turns the truck to the right
+     */
     @Override
     public void turnRight() {
         this.movable.rotate(-10);
