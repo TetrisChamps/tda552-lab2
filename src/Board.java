af9bdb1597f08b1c798e13c326e540074c0b1d48
Rasmus Rosengren <rsrp98@gmail.com>
2018-11-28 14:20:00 +0100

    Comments

--- src/Board.java
+++ src/Board.java
@@ -1,14 +1,25 @@
 public class Board {
-    private int angle;
+    private int angle; // The angle of the board, 0 = down
 
+    /**
+     * Raises the board
+     */
     public void raise() {
         angle = Math.min(70, angle + 10);
     }
 
+    /**
+     * Lowers the board
+     */
     public void lower() {
         angle = Math.max(0, angle - 10);
     }
 
+    /**
+     * Checks if the board is down
+     *
+     * @return
+     */
     public boolean isDown() {
         return angle == 0;
     }
---
+++ test/CarFerryTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarFerryTest {
+
+    CarFerry carFerry;
+
+    @BeforeEach
+    void setUp() {
+        carFerry = new CarFerry(Color.BLUE, "ferry1",10, 5, 7);
+    }
+
+    @AfterEach
+    void tearDown() {
+        carFerry = null;
+    }
+
+    @Test
+    void addCar() {
+        Car car = new Saab95();
+        carFerry.addCar(car);
+
+        carFerry.dock();
+        assertEquals(carFerry.getCars().size(), 1);
+
+        carFerry.unDock();
+        Car car1 = new Volvo240();
+
+        carFerry.addCar(car1);
+        assertEquals(carFerry.getCars().size(), 1);
+
+    }
+
+    @Test
+    void removeCar() {
+    }
+
+    @Test
+    void dock() {
+    }
+
+    @Test
+    void unDock() {
+    }
+}
