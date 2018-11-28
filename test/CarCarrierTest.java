b5aeeaf7c23330100bf0067a21450caf3d7ef9b8
Rasmus Rosengren <rsrp98@gmail.com>
2018-11-28 15:55:53 +0100

    carcarrier tests: getcars and removecar

--- test/CarCarrierTest.java
+++ test/CarCarrierTest.java
@@ -29,9 +29,21 @@ class CarCarrierTest {
 
     @Test
     void removeCar() {
+        Car transporter = new Volvo240();
+        assertEquals(null, carrier.removeCar(true, transporter));
+        Car c = new Saab95();
+        carrier.addCar(c, transporter);
+        assertEquals(c, carrier.removeCar(false, transporter));
     }
 
     @Test
     void getCars() {
+        assertEquals(carrier.getCars().size(), 0);
+        Car transporter = new Volvo240();
+        Car c1 = new Saab95();
+        carrier.addCar(c1, transporter);
+        Car c2 = new Saab95();
+        carrier.addCar(c2, transporter);
+        assertEquals(carrier.getCars().size(), 2);
     }
 }
