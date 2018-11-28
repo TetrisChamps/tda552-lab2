b139ebcbb8cfe2028ee480fda0c146ce29a62c94
Carl Holmberg <carl.j.holmberg@telia.com>
2018-11-28 16:06:06 +0100

    CarCarrierTest

--- test/CarCarrierTest.java
+++ test/CarCarrierTest.java
@@ -2,7 +2,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CarCarrierTest {
 
@@ -20,11 +21,30 @@ class CarCarrierTest {
 
     @Test
     void move() {
-
+        CarTransporter transporter = new CarTransporter();
+        carrier.addCar(new Volvo240(), transporter);
+        carrier.addCar(new Volvo240(), transporter);
+        carrier.move(10, 5);
+        assertEquals(10, carrier.getCars().get(0).getX());
+        assertEquals(5, carrier.getCars().get(0).getY());
+        assertEquals(10, carrier.getCars().get(1).getX());
+        assertEquals(5, carrier.getCars().get(1).getY());
     }
 
     @Test
     void addCar() {
+        CarTransporter transporter = new CarTransporter();
+        assertThrows(IllegalArgumentException.class, () -> carrier.addCar(transporter, transporter), "Cannot add itself to itself");
+        Car c = new Saab95();
+        carrier.addCar(c, transporter);
+        Scania scania = new Scania();
+        assertThrows(IllegalStateException.class, () -> carrier.addCar(scania, transporter), "Car is too heave");
+        assertThrows(IllegalStateException.class, () -> carrier.addCar(c, transporter), "Car is already loaded");
+        carrier = new CarCarrier(5, 2000);
+        for (int i = 0; i < 5; i++) {
+            carrier.addCar(new Saab95(), transporter);
+        }
+        assertThrows(IllegalStateException.class, () -> carrier.addCar(new Saab95(), transporter));
     }
 
     @Test
